4fa7a5ca11d4be2fc794a45a8562d0dab470de94
Fatima   Rajab <112514257+fatimarajab12@users.noreply.github.com>
2025-10-15 21:21:33 +0300

    new project structer

---
+++ cypress/support/commands.ts
@@ -0,0 +1,67 @@
+/// <reference types="cypress" />
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+// declare global {
+//   namespace Cypress {
+//     interface Chainable {
+//       login(email: string, password: string): Chainable<void>
+//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+//     }
+//   }
+// }
+
+    
+    Cypress.Commands.add('loginToOrangeHRM',()=>{
+     cy.visit("https://opensource-demo.orangehrmlive.com")
+        cy.wait(1000)
+        cy.fixture("dataLoginToOrangeHRM").then((user)=>{
+          cy.get("[name=username]").type(user.email)
+         cy.wait(1000)
+        cy.get("[type=password]").type(user.password)
+})
+        
+        cy.get(".oxd-button").click()
+})
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      loginToOrangeHRM(): Chainable<void>;
+    }
+  }
+}
+
+export {};
+
+
+
+
+
+
+
+
