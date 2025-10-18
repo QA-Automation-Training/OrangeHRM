551e6de0ec673c894bb2fec8c7c42d46ecbb4232
Fatima   Rajab <112514257+fatimarajab12@users.noreply.github.com>
2025-10-18 22:23:16 +0300

    employee list And fillters

--- cypress/e2e/pages/pim/employee-list.page.ts
+++ cypress/e2e/pages/pim/employee-list.page.ts
@@ -1,43 +1,106 @@
-// class EmployeeListPage {
-//   elements = {
-//     pimMenu: () => cy.get(".oxd-main-menu-item-wrapper").contains("PIM"),
-//     employeeListTab: () => cy.get(".oxd-topbar-body-nav-tab-item").contains("Employee List"),
-//     employeeName: () => cy.get('input[placeholder="Type for hints..."]'),
-//     employeeIdInput: () =>
-//       cy.get('label')
-//         .contains('Employee Id')
-//         .closest('.oxd-input-group')//move to upper level on step 
-//         .find('input'),
-
-//     searchButton: () => cy.get('button[type="submit"]'),
+class EmployeeListPage {
+  elements = {
+    pimMenu: () => cy.get(".oxd-main-menu-item-wrapper").contains("PIM"),
+    employeeListTab: () => cy.get(".oxd-topbar-body-nav-tab-item").contains("Employee List"),
 
 
-//   };
+    // '.oxd-table-filter-area' is the element that wraps all the filter fields
+    // The callback gives access to the jQuery element ($el)
+    // '.oxd-table-filter-header-options .--toggle button' is the button that opens the filter area
+    // This ensures all input fields inside the filter become visible and interactable
+    // This prevents the test from failing if the element is not yet loaded
 
-//   navigateToEmployeeList(): this {
-//     // TODO: Click PIM menu
-//     this.elements.pimMenu().click();
-//     this.elements.employeeListTab().click();
-//     cy.url().should("include", "/pim/viewEmployeeList");
-//     this.waitForLoading();
-//     return this;
+    openFilttersArea: () => cy.get('.oxd-table-filter-area', { timeout: 5000 })
+      .should('exist')
+      .then($el => {
+        if ($el.css('display') === 'none') {
+          cy.get('.oxd-table-filter-header-options .--toggle button').click();
+        }
+      }),
 
-//   }
-//   waitForLoading() {
-//     cy.get('.oxd-loading-spinner').should('not.exist');
-//   }
+    employeeName: () => cy.get('label')
+      .contains('Employee Name')
+      .closest('.oxd-input-group')
+      .find('input'),
 
-//   searchEmployee(employeeName): this {
-//     this.elements.employeeName().clear().type(employeeName);
-//     // TODO: Click search button
-//     this.elements.searchButton().click();
-//     return this;
-//   }
 
-//   verifyEmployeeInList(employeeName): this {
-//     // TODO: Check that search results have at least 1 row
-//     // TODO: Check that employee name exists in results
-//   }
-// }
+    employeeIdInput: () =>
+      cy.get('label')
+        .contains('Employee Id')
+        .closest('.oxd-input-group')
+        .find('input'),
 
-// export default EmployeeListPage;
+    employmentStatusDropdown: () =>
+      cy.contains('label', 'Employment Status')
+        .closest('.oxd-input-group')
+        .find('.oxd-select-text-input'),
+
+    dropdownOption: () => cy.get('.oxd-select-dropdown'),
+
+    includeDropdown: () =>
+  cy.contains('label', 'Include')
+    .closest('.oxd-input-group')
+    .find('.oxd-select-text-input'),
+
+
+jobTitleDropdown: () =>
+  cy.contains('label', 'Job Title')
+    .closest('.oxd-input-group')
+    .find('.oxd-select-text-input'),
+
+
+    searchButton: () => cy.get('button[type="submit"]'),
+
+
+
+
+  };
+
+  navigateToEmployeeList(): this {
+    this.elements.pimMenu().click();
+    this.elements.employeeListTab().click();
+    cy.url().should("include", "/pim/viewEmployeeList");
+    this.waitForLoading();
+    return this;
+
+  }
+  waitForLoading() {
+    cy.get('.oxd-loading-spinner').should('not.exist');
+  }
+
+  searchEmployeeBYName(employeeName): this {
+    this.elements.employeeName().clear().type(employeeName);
+    this.elements.searchButton().click();
+    return this;
+  }
+  searchEmployeeBYId(employeeId): this {
+    this.elements.employeeIdInput().clear().type(employeeId);
+    this.elements.searchButton().click;
+    return this;
+
+  }
+
+// This method dynamically selects a dropdown option based on its visible text.
+// We locate the dropdown using its <label> (more stable than using index or order),
+// then click to open it and choose the matching option by content.
+// This approach is flexible and works even if the DOM structure or element order changes.
+   selectEmploymentStatus(status) {
+    this.elements.employmentStatusDropdown().click(); 
+    this.elements.dropdownOption().contains(status).click(); 
+  }
+selectEmployeeIncludeOption(option) {
+    this.elements.includeDropdown().click(); 
+    this.elements.dropdownOption().contains(option).click(); 
+  }
+
+  selectEmloyeeJobTitle(title) {
+    this.elements.jobTitleDropdown().click(); 
+    this.elements.dropdownOption().contains(title).click(); 
+  }
+
+
+
+  
+}
+
+export default EmployeeListPage;
--- cypress/e2e/specs/pim/employee-list.page.spec.ts
+++ cypress/e2e/specs/pim/employee-list.page.spec.ts
@@ -1,16 +1,46 @@
+import EmployeeListPage from "../../pages/pim/employee-list.page";
+const employeeListPage = new EmployeeListPage();
 
-// import EmployeeListPage from "../../pages/pim/employee-list.page";
-// const employeeListPage = new EmployeeListPage();
+describe("PIM Employee List Page", () => { 
+  beforeEach(() => {  
+    cy.loginToOrangeHRM();
+    employeeListPage.navigateToEmployeeList();
+    employeeListPage.elements.openFilttersArea();
+  });
 
-// describe("PIM Employee List Page", () => {  
-//     beforeEach(() => {  
-//         cy.loginToOrangeHRM();
-//         employeeListPage.navigateToEmployeeList();
-//     });
-//     it("should search for an employee by name", () => { 
-//         employeeListPage.searchEmployee("John Doe");
-           
-        
-//     });
+  it("should search for an employee by name", () => { 
+    employeeListPage.searchEmployeeBYName("John Smith");
+  });
 
-// });
+  it("should search for an employee by ID", () => { 
+    employeeListPage.searchEmployeeBYId("12345");
+  });
+
+  it('should filter employees by employment status', () => {
+    cy.fixture("employmentStatus").then((data) => {
+      data.statuses.forEach((status) => {
+        employeeListPage.selectEmploymentStatus(status);
+        // Optional: verify results
+        cy.get('.oxd-table-row').should('exist');
+      });
+    });
+  });
+
+  it('should filter employees by include option', () => {
+    cy.fixture("includeOptions").then((data) => {
+      data.options.forEach(option => {
+        employeeListPage.selectEmployeeIncludeOption( option)  // Page Object method
+        cy.get('.oxd-table-row').should('exist');
+      });
+    });
+  });
+    it('should filter employees by job title', () => {
+        cy.fixture("jobTitles").then((data) => {
+            data.titles.forEach(title => {
+                employeeListPage.selectEmloyeeJobTitle(title);  // Page Object method
+                cy.get('.oxd-table-row').should('exist');
+            });
+        });
+    });
+
+});
