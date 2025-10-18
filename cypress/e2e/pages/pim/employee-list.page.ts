e82f5a58f951be5524d09227f6737b89ee3e1d2a
Fatima   Rajab <112514257+fatimarajab12@users.noreply.github.com>
2025-10-18 11:18:51 +0300

    github actions

--- cypress/e2e/pages/pim/employee-list.page.ts
+++ cypress/e2e/pages/pim/employee-list.page.ts
@@ -1,30 +1,43 @@
-class EmployeeListPage {
-  elements = {
-  employeeName: ()=>cy.get('input[placeholder="Type for hints..."]'),  
-employeeIdInput: () =>
-    cy.get('label')
-      .contains('Employee Id')
-      .closest('.oxd-input-group')//move to upper level on step 
-      .find('input'),
+// class EmployeeListPage {
+//   elements = {
+//     pimMenu: () => cy.get(".oxd-main-menu-item-wrapper").contains("PIM"),
+//     employeeListTab: () => cy.get(".oxd-topbar-body-nav-tab-item").contains("Employee List"),
+//     employeeName: () => cy.get('input[placeholder="Type for hints..."]'),
+//     employeeIdInput: () =>
+//       cy.get('label')
+//         .contains('Employee Id')
+//         .closest('.oxd-input-group')//move to upper level on step 
+//         .find('input'),
 
-    
-    
-  };
+//     searchButton: () => cy.get('button[type="submit"]'),
 
-  navigateToEmployeeList(): this {
-    // TODO: Click PIM menu
-    // TODO: Wait for employee list page URL
-  }
 
-  searchEmployee(employeeName): this {
-    // TODO: Type employee name in search input
-    // TODO: Click search button
-  }
+//   };
 
-  verifyEmployeeInList(employeeName): this {
-    // TODO: Check that search results have at least 1 row
-    // TODO: Check that employee name exists in results
-  }
-}
+//   navigateToEmployeeList(): this {
+//     // TODO: Click PIM menu
+//     this.elements.pimMenu().click();
+//     this.elements.employeeListTab().click();
+//     cy.url().should("include", "/pim/viewEmployeeList");
+//     this.waitForLoading();
+//     return this;
 
-export default EmployeeListPage;
+//   }
+//   waitForLoading() {
+//     cy.get('.oxd-loading-spinner').should('not.exist');
+//   }
+
+//   searchEmployee(employeeName): this {
+//     this.elements.employeeName().clear().type(employeeName);
+//     // TODO: Click search button
+//     this.elements.searchButton().click();
+//     return this;
+//   }
+
+//   verifyEmployeeInList(employeeName): this {
+//     // TODO: Check that search results have at least 1 row
+//     // TODO: Check that employee name exists in results
+//   }
+// }
+
+// export default EmployeeListPage;
--- cypress/e2e/specs/pim/employee-list.page.spec.ts
+++ cypress/e2e/specs/pim/employee-list.page.spec.ts
@@ -1,2 +1,16 @@
 
-import EmployeeListPage from "../../pages/pim/employee-list.page";
+// import EmployeeListPage from "../../pages/pim/employee-list.page";
+// const employeeListPage = new EmployeeListPage();
+
+// describe("PIM Employee List Page", () => {  
+//     beforeEach(() => {  
+//         cy.loginToOrangeHRM();
+//         employeeListPage.navigateToEmployeeList();
+//     });
+//     it("should search for an employee by name", () => { 
+//         employeeListPage.searchEmployee("John Doe");
+           
+        
+//     });
+
+// });
