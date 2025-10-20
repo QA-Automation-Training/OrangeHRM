dc8684aa8b2f2d0651219298dda13613fb36f8ae
Fatima   Rajab <112514257+fatimarajab12@users.noreply.github.com>
2025-10-20 20:00:19 +0300

    Fix Cypress configuration and support files structure

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,28 +1,20 @@
 import { defineConfig } from "cypress";
-import {
-  addCucumberPreprocessorPlugin,
-} from '@badeball/cypress-cucumber-preprocessor';
-
 
 export default defineConfig({
   e2e: {
-    
     baseUrl: "https://opensource-demo.orangehrmlive.com",
-    specPattern: "cypress/e2e/specs/**/*.spec.ts",
+    specPattern: "cypress/e2e/**/*.spec.ts",
     supportFile: "cypress/support/e2e.ts",
     fixturesFolder: "cypress/fixtures",
-    viewportWidth: 900,
+    viewportWidth: 1920,
     viewportHeight: 1080,
     defaultCommandTimeout: 10000,
     pageLoadTimeout: 60000,
-    responseTimeout: 30000,
-    requestTimeout: 5000,
     video: false,
     screenshotOnRunFailure: true,
-    retries: {
-      runMode: 1,
-      openMode: 0
-    },
     
+    setupNodeEvents(on, config) {
+      return config;
+    }
   }
 });
