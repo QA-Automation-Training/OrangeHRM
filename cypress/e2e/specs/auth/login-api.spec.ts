cf57b92e5d08dfba547afa696e2d11eae0d30fb8
Fatima   Rajab <112514257+fatimarajab12@users.noreply.github.com>
2025-10-21 18:14:25 +0300

    sign in as admin via api

--- cypress/e2e/specs/auth/login-api.spec.ts
+++ cypress/e2e/specs/auth/login-api.spec.ts
@@ -1,15 +1,15 @@
 describe('OrangeHRM API Login', () => {
   it('should log in successfully', () => {
-    cy.request({
-      method: 'POST',
-      url: 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/validate',
-      body: {
-        username: 'Admin',
-        password: 'admin123'
-      }
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      cy.log('Login successful');
-    });
+   cy.request({
+  method: 'POST',
+  url: 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/validate',
+  form: true,
+  body: { username: 'Admin', password: 'admin123' },
+  followRedirect: false
+}).then((response) => {
+  expect([200, 302]).to.include(response.status);
+  cy.log('Login successful, redirect or dashboard loaded');
+});
+
   });
 });
